0aa389cb2234320641c9802390e54f1bf1ab497f
Sofia Linares <sofia.linares25@gmail.com>
2022-03-21 01:42:13 -0300

    modificaciones

--- js/app.js
+++ js/app.js
@@ -137,3 +137,14 @@ function alertaremovido (){
     swal("Carrito vacio")
 }
 
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#xdmail')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `mailto:sofia.linares25@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+}
